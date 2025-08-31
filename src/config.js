713f4104577f5273eb9b28c8b61d071e807b079e
falcmarc <142171753+falcmarc@users.noreply.github.com>
2025-08-31 13:20:03 +0200

    correzione USDA

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 // src/config.js
-// Per USDA serve una API key (gratuita): https://fdc.nal.usda.gov/api-key-signup.html
-// Per la demo puoi lasciare stringa vuota (il provider USDA verrà saltato)
-export const CONFIG = {
-  USDA_API_KEY: "",   // <-- metti qui la tua chiave quando la avrai
-  NUTRITION_CACHE_DAYS: 30,
+export default {
+  USDA_API_KEY: '', // opzionale, se non hai la chiave lascia stringa vuota
 };
